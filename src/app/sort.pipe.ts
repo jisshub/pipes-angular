4bbafac36fa7dae6298f9c42d3116894099788e7
jissmon <jisshub>
2020-10-07 18:44:49 +0530

    pipe to sort the server name- not working

--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class SortPipe implements PipeTransform {
   transform(value: any, serverName: string): any {
     for (const item of value) {
-      console.log(item[serverName]);
+      // console.log(item[serverName]);
     }
   }
 }
